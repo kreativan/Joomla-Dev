1b59324686e6c0b2bbc78536072065cb251bcce8
Ivan Milincic <lokomotivan@gmail.com>
2015-10-24 20:58:16 +0200

    Create mod_spotlight_news.php

---
+++ mod_spotlight_news/mod_spotlight_news.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package     Joomla.Module
+ * @subpackage  mod_spotlight_news
+ * @link        http://www.kreativan.net
+ * @copyright   Ivan Milincic - www.kreativan.net. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+// Include the syndicate functions only once
+require_once dirname(__FILE__) . '/helper.php';
+
+// Get Articles from db
+if ($params->get('source') == 'joomla') {
+// Get Joomla Articles from db
+//$sort = 'created DESC';
+$sort = $params->get('sorting');
+$no_of_articles = $params->get('no_of_items');
+$db = JFactory::getDbo();
+$query = $db->getQuery(true);
+
+$query->select('*')
+        ->from('#__content')
+        ->order($sort)
+        ->setLimit($no_of_articles);
+
+$db->setQuery($query);
+$articles = $db->loadObjectList(); 
+}
+ 
+$spotlightnews = modSpotlightNewHelper::getSpotlightNew($params);
+require JModuleHelper::getLayoutPath('mod_spotlight_news');
