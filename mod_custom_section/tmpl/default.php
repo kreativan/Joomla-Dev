3c8d3845089732073a4b337c012869c47a91f969
Ivan Milincic <lokomotivan@gmail.com>
2015-11-06 17:47:22 +0100

    added overlay

--- mod_custom_section/tmpl/default.php
+++ mod_custom_section/tmpl/default.php
@@ -12,6 +12,7 @@ defined('_JEXEC') or die;
     $module_id = $module->id;
 
     $ivm_bg                 = $params->get('background');
+    $ivm_overlay            = $params->get('overlay');
 
     $ivm_class              = ' ' . $params->get('class');
     $ivm_css_id             = 'id="' . $params->get('css_id') . '"';  
@@ -32,10 +33,16 @@ defined('_JEXEC') or die;
 
 ?>
 
-<div <?php echo $ivm_css_id;?> class="ivm-custom-section<?php echo $ivm_class;?>" style="background:url(<?php echo $ivm_bg;?>);background-size:cover;background-repeat:no-repeat;background-position:center;padding:<?php echo $ivm_content_padding;?> 0;">
-    <div class="uk-container uk-container-center <?php echo $ivm_full_screen;?> uk-flex uk-flex-<?php echo $ivm_h_align;?> uk-flex-<?php echo $ivm_v_align;?>" style="min-height:<?php echo $ivm_height;?>;">
+<div <?php echo $ivm_css_id;?> class="ivm-custom-section<?php echo $ivm_class;?>" style="background:url(<?php echo $ivm_bg;?>);background-size:cover;background-repeat:no-repeat;background-position:center;padding:<?php echo $ivm_content_padding;?> 0;position:relative;">
+    
+    <?php if ($ivm_overlay == 1) : ?>
+    <div class="ivm-overlay" style="position:absolute;top:0;bottom:0;left:0;right:0;background: rgba(0, 0, 0, 0.5);z-index:1;"></div>
+    <?php endif;?>
+    
+    <div class="uk-container uk-container-center <?php echo $ivm_full_screen;?> uk-flex uk-flex-<?php echo $ivm_h_align;?> uk-flex-<?php echo $ivm_v_align;?>" style="min-height:<?php echo $ivm_height;?>;position:relative;z-index:9;">
         <div class="ivm-custom-section-content" style="width:<?php echo $ivm_content_size;?>">
             <?php echo $module->content;?>
         </div>
     </div>
+    
 </div>
