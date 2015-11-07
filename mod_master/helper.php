7494d9e92c42eb96eedcd8febdc86481d9768993
Ivan Milincic <lokomotivan@gmail.com>
2015-11-08 00:40:23 +0100

    rearange

--- mod_master/helper.php
+++ mod_master/helper.php
@@ -17,3 +17,48 @@ class ModMasterHelper {
         
     }
 }
+
+// Get items from repeatable fields
+/*
+// decode json into array
+$ivm = $params->get('repeatable_field_name'); // <--change
+$json = json_decode($ivm, true);
+
+// if array exist and not empty
+if (is_array($json)) {
+    // function name must be diferent for each module (replace 'my_items' with anything you wont)
+    // checks if function allready exist
+    if (!function_exists('my_items')) { // <--change
+        function my_items($array) { // <--change
+            $result = array();
+            foreach ($array as $sub) {
+                foreach ($sub as $k => $v) {
+                    $result[$k][] = $v;
+                }
+            }
+            return $result;
+        }
+    }
+    $ivm_array = my_items($json); // <--change
+    $items     = $ivm_array; // this is default items array
+}
+*/
+
+// Get Articles from db
+/*
+//$sort = 'created DESC';
+$sort = $params->get('sorting');
+$no_of_articles = $params->get('no_of_items');
+$catId = $params->get('mycategory');
+$db = JFactory::getDbo();
+$query = $db->getQuery(true);
+
+$query->select('*')
+        ->from('#__content')
+        ->where("catid ='" . $catId . "'")
+        ->order($sort)
+        ->setLimit($no_of_items);
+
+$db->setQuery($query);
+$articles = $db->loadObjectList();
+*/
--- mod_master/mod_master.php
+++ mod_master/mod_master.php
@@ -16,53 +16,11 @@ $document = JFactory::getDocument();
        
 // add custom js
 //$document->addScript(JURI::base(true) . '/modules/mod_master/assets/js/script.js');
+// add custom css
 //$document->addStylesheet(JURI::base(true) . '/modules/mod_master/assets/css/style.css');
 
-
-// Get items from repeatable fields
-/*
-// decode json into array
-$ivm = $params->get('repeatable_field_name'); // <--change
-$json = json_decode($ivm, true);
-
-// if array exist and not empty
-if (is_array($json)) {
-    // function name must be diferent for each module (replace 'my_items' with anything you wont)
-    // checks if function allready exist
-    if (!function_exists('my_items')) { // <--change
-        function my_items($array) { // <--change
-            $result = array();
-            foreach ($array as $sub) {
-                foreach ($sub as $k => $v) {
-                    $result[$k][] = $v;
-                }
-            }
-            return $result;
-        }
-    }
-    $ivm_array = my_items($json); // <--change
-    $items     = $ivm_array; // this is default items array
-}
-*/
-
-// Get Articles from db
-/*
-//$sort = 'created DESC';
-$sort = $params->get('sorting');
-$no_of_articles = $params->get('no_of_items');
-$catId = $params->get('mycategory');
-$db = JFactory::getDbo();
-$query = $db->getQuery(true);
-
-$query->select('*')
-        ->from('#__content')
-        ->where("catid ='" . $catId . "'")
-        ->order($sort)
-        ->setLimit($no_of_items);
-
-$db->setQuery($query);
-$articles = $db->loadObjectList();
-*/
+$id			        = $module->id;
+$kreativan			= JText::_('KREATIVAN_NET');
  
 $master = modMasterHelper::getMaster($params);
-require JModuleHelper::getLayoutPath('mod_master');
+require JModuleHelper::getLayoutPath('mod_master', $params->get('layout', 'default'));
