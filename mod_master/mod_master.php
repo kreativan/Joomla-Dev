77e20030fa6648e20de161bfb6ac4fcb5766fd23
Ivan Milincic <lokomotivan@gmail.com>
2015-11-12 23:32:58 +0100

    update

--- mod_master/mod_master.php
+++ mod_master/mod_master.php
@@ -15,12 +15,11 @@ require_once dirname(__FILE__) . '/helper.php';
 $document = JFactory::getDocument();
        
 // add custom js
-//$document->addScript(JURI::base(true) . '/modules/mod_master/assets/js/script.js');
+//$document->addScript(JURI::base(true) . '/modules/mod_master/assets/script.js');
 // add custom css
-//$document->addStylesheet(JURI::base(true) . '/modules/mod_master/assets/css/style.css');
+//$document->addStylesheet(JURI::base(true) . '/modules/mod_master/assets/style.css');
 
-$id			        = $module->id;
-$kreativan			= JText::_('KREATIVAN_NET');
+$moduleclass_sfx    = $params->get('moduleclass_sfx');
  
 $master = modMasterHelper::getMaster($params);
 require JModuleHelper::getLayoutPath('mod_master', $params->get('layout', 'default'));
