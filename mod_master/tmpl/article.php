6be2aaccf8bb76e7df4bd4aaa22a7da6845d3d72
Ivan Milincic <lokomotivan@gmail.com>
2015-11-14 16:05:27 +0100

    update

--- mod_master/tmpl/article.php
+++ mod_master/tmpl/article.php
@@ -10,15 +10,35 @@
 // No direct access
 defined('_JEXEC') or die;
 
+    $module_id = $module->id;
+
+    $limit  = 0;
+
 ?>
 
-<!-- Loop true articles -->
-<?php if (is_array($articles)) : ?><!-- if array exists -->
+<!-- Loop true categories -->
+<?php foreach($categories as $category) : ?>
+<?php
+        // get categories from db
+        $db = JFactory::getDbo();
+        $query = $db->getQuery(true);
 
-  <?php foreach($articles as $article) : ?>
-  <?php if($article->state !== '0') : ?>
+        $query->select('*')
+                ->from('#__content')
+                ->where("catid ='" . $category . "'")
+                ->order($articles_sort)
+                ->setLimit($no_of_articles);
+
+        $db->setQuery($query);
+        $articles = $db->loadObjectList();
+    
+?> 
+
+    <!-- Loop true articles -->
+    <?php if (is_array($articles)) : ?><!-- if array exists -->
+    <?php foreach($articles as $article) : ?>
+    <?php if($article->state !== '0') : ?>
     <?php  
-        // ARTICLE VARIABLES
 
         // get images array from json
         $json = $article->images;
@@ -40,30 +60,53 @@ defined('_JEXEC') or die;
         $url = JRoute::_(ContentHelperRoute::getArticleRoute($article->slug, $article->catid, $article->language));
       
         // FINAL ARTICLE VARIABLES
-        // images
-        $intro_image = $image->{'image_intro'};
-        $full_image = $image->{'image_fulltext'};
         // Text
         $title = $article->title;
         $intro_text = $article->introtext;
+        $full_text  = $article->fulltext; 
+    
+        // images
+        $intro_image = $image->{'image_intro'};
+        $full_image = $image->{'image_fulltext'};
+        //content images
+        preg_match('/<img.+src=[\'"](?P<src>.+)[\'"].*>/i', $intro_text, $intro_img);
+        $intro_content_image = $intro_img['src'];
+        preg_match('/<img.+src=[\'"](?P<src>.+)[\'"].*>/i', $full_text, $full_img);
+        $full_content_image = $full_img['src'];
+    
         // Meta
         $date = JHtml::_('date', $article->created, JText::_('d F Y'));
         $category = $category_title;
         $author = $username;
         $meta = $date . $category . $author;
+    
         // URL
         $url = JRoute::_(ContentHelperRoute::getArticleRoute($article->slug, $article->catid, $article->language));
         
+        // check for image
         if ($intro_image) {
             $image = $intro_image;
-        }else {
+        }
+        elseif($full_image) {
             $image = $full_image;
         }
+        elseif($intro_content_image) {
+            $image = $intro_content_image;
+        }else {
+            $image = $full_content_image;
+        }
+
+        // limit number of articles per category
+        if($limit == $no_of_articles) break;
+        $limit++;
       
         print_r($article);
     ?>
+
+    <!-- LOOP ITEMS -->
     
   <?php endif;?>    
   <?php endforeach; ?>
-
-<?php endif;?>
+  <?php endif;?>
+            
+<?php endforeach; ?>
