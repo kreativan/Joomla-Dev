38ed888f40e8f195342114412e7885623c46a031
Ivan Milincic <lokomotivan@gmail.com>
2016-01-05 23:05:26 +0100

    Update db_query.php

--- @Joomla-Code/db_query.php
+++ @Joomla-Code/db_query.php
@@ -1,21 +1,25 @@
 <?php
-// Connect to database
-$db = JFactory::getDbo();
-$query = $db->getQuery(true);
- 
-// Build the query
-$query->select($db->quoteName(array('title', 'introtext')));
-$query->from($db->quoteName('#__content'));
-$query->where($db->quoteName('introtext') . ' LIKE '. $db->quote('%Joomla%'));
-$query->order('ordering ASC');
-$query->setLimit('1');
- 
-$db->setQuery($query);
-$results = $db->loadObjectList();
+    // Get Articles from db
+    $sort = $params->get('sorting');
+    $no_of_articles = '10';
+    $catId = join(',',$params->get('catid')); // <-- multiple categories
+    // $catId = $params->get('catid'); // <-- single category
+    $db = JFactory::getDbo();
+    $query = $db->getQuery(true);
 
-// Print the result
-foreach($results as $result){
-    echo '<h3>' . $result->title . '</h3>';
-    echo $result->introtext;
-}
+    $query->select('*')
+            ->from('#__content')
+            //->where("catid ='" . $catId . "'") <-- single category
+            ->where("category IN ($catId) AND state = '1'") // <-- multiple categories + if published
+            ->order($sort)
+            ->setLimit($no_of_articles);
+
+    $db->setQuery($query);
+    $items = $db->loadObjectList();
+?>
+
+<?php 
+  foreach($items as $item) {
+  // loop
+  }
 ?>
