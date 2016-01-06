4db30f9c6b6fd68d1889d6cd346f88f27f4553fd
Ivan Milincic <lokomotivan@gmail.com>
2016-01-06 22:41:55 +0100

    Update db.php

--- @Joomla-Code/db.php
+++ @Joomla-Code/db.php
@@ -1,25 +1,35 @@
 <?php
+
     // Get Articles from db
     $sort = $params->get('sorting');
-    $no_of_articles = '10';
-    $catId = join(',',$params->get('catid')); // <-- multiple categories
-    // $catId = $params->get('catid'); // <-- single category
+    $no_of_articles = $params->get('no_of_items');
+    
+    //$catId = $params->get('catid');                           // <-- single category
+    $catId = join(',', $params->get('catid'));                  //<-- join multiple categories
+
+    // Check if 'All Categories are selected'
+    if (in_array('all', $params->get('catid'))) {
+        $sql_where = "state = '1'";
+    }else {
+        $sql_where = "catid IN ($catId) AND state='1'";
+    }
+
     $db = JFactory::getDbo();
     $query = $db->getQuery(true);
-
     $query->select('*')
             ->from('#__content')
-            //->where("catid ='" . $catId . "'") <-- single category
-            ->where("category IN ($catId) AND state = '1'") // <-- multiple categories + if published
+            //->where("catid = '$catId' AND state='1'")         // <-- single category
+            ->where($sql_where)                                 // <-- multiple cateogries
             ->order($sort)
             ->setLimit($no_of_articles);
 
     $db->setQuery($query);
-    $items = $db->loadObjectList();
+    $articles = $db->loadObjectList();
+
 ?>
 
 <?php 
-  foreach($items as $item) {
+  foreach($articles as $article) {
   // loop
   }
 ?>
