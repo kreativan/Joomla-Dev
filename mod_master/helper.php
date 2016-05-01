00f14a7b710e2ef547e4ce56308c090f081f7dd6
Ivan Milincic <lokomotivan@gmail.com>
2016-05-01 20:33:28 +0200

    xml update

--- mod_master/helper.php
+++ mod_master/helper.php
@@ -6,33 +6,81 @@
  * @copyright   Ivan Milincic - www.kreativan.net. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
- 
+
 // No direct access
 defined('_JEXEC') or die;
 
 class ModMasterHelper {
-   
+
     public static function getMaster($params) {
-       
-        
+
+
     }
 }
 
-// Get Articles from db
-/*
-//$sort = 'created DESC';
-$sort = $params->get('sorting');
-$no_of_articles = $params->get('no_of_items');
-$catId = $params->get('mycategory');
-$db = JFactory::getDbo();
-$query = $db->getQuery(true);
+/* GET ARTICLES FROM DB
+===================================================================================================== */
+if($params->get('source') == 'joomla') {
 
-$query->select('*')
-        ->from('#__content')
-        ->where("catid ='" . $catId . "'")
-        ->order($sort)
-        ->setLimit($no_of_items);
+    // Get Articles from db
+    $sort = $params->get('sorting');
+    $no_of_articles = $params->get('no_of_j_items');
 
-$db->setQuery($query);
-$articles = $db->loadObjectList();
-*/
+    //$catId = $params->get('catid');                           // <-- single category
+    $catId = join(',', $params->get('catid'));                  //<-- join multiple categories
+
+    // Check if 'All Categories are selected'
+    if (in_array('all', $params->get('catid'))) {
+        $sql_where = "state = '1'";
+    }else {
+        $sql_where = "catid IN ($catId) AND state='1'";
+    }
+
+    $db = JFactory::getDbo();
+    $query = $db->getQuery(true);
+    $query->select('*')
+            ->from('#__content')
+            //->where("catid = '$catId' AND state='1'")         // <-- single category
+            ->where($sql_where)                                 // <-- multiple cateogries
+            ->order($sort)
+            ->setLimit($no_of_articles);
+
+    $db->setQuery($query);
+    $articles = $db->loadObjectList();
+
+}
+
+/* REPEATER ARRAY
+===================================================================================================== */
+if($params->get('source') == 'custom') {
+
+    $ivm = $params->get('items'); // <--change
+    $json = json_decode($ivm, true);
+    if (is_array($json)) {
+        if (!function_exists('items')) { // <--change
+            function items($array) { // <--change
+                $result = array();
+                foreach ($array as $sub) {
+                    foreach ($sub as $k => $v) {
+                        $result[$k][] = $v;
+                    }
+                }
+                return $result;
+            }
+        }
+        $ivm_array = items($json); // <--change
+        $items     = $ivm_array; // this is default items array
+        $items = array_reverse($items); // reverse sorting
+    }
+    if (!function_exists('sorting')) {
+
+            function sorting($a, $b) {
+                return $a[0] - $b[0];
+            }
+        }
+
+    if($params->get('items')) {
+        usort($items, 'sorting');
+    }
+
+}
